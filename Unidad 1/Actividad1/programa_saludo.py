9810bdf9282c18f906f13718c92f2b7c1b8cfe39
TanailyAlcala <l21100152@nlaredo.tecnm.mx>
2025-09-09 17:54:14 -0500

    Actividad 1 - programa_saludo

---
+++ Unidad 1/Actividad1/programa_saludo.py
@@ -0,0 +1,13 @@
+## Actividad 1 - Unidad 1
+# Tanaily Alcala Barraza 21100152
+
+# --- Parte A. a ---
+print("Hola, este es mi primer programa en Python")
+
+# --- Parte A. b ---
+# Solicitar datos al usuario
+nombre = input("Escribe tu nombre: ")
+edad = input("Escribe tu edad: ")
+
+# Mostrar mensaje
+print(f"Hola {nombre}, tienes {edad} años")
